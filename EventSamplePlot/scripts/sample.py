6486ffade32fa877977b6ef795c2765be6974eba
qingyuanzhang3 <zhangqingyuan03@icloud.com>
2022-01-15 11:52:46 +1000

    Update sample.py

--- EventSamplePlot/scripts/sample.py
+++ EventSamplePlot/scripts/sample.py
@@ -8,6 +8,6 @@ mf, sampler = fitting.fit_event(snakemake.params.year,
                                 cbm_model=snakemake.params.cbm_model,
                                 production_model="flexible_sinusoid",
                                 hemisphere=snakemake.params.hemisphere,
-                                sampler="MCMC", burnin=10, production=101,
+                                sampler="MCMC", burnin=2000, production=2000,
                                 oversample=108)
 np.save(snakemake.output[0], sampler)
