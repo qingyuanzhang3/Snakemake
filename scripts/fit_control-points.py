dd1945a6b5d27676dab0a1d2a97526f2b847d276
qingyuanzhang3 <zhangqingyuan03@icloud.com>
2022-01-28 10:16:12 +1000

    Update fit_control-points.py

--- scripts/fit_control-points.py
+++ scripts/fit_control-points.py
@@ -3,7 +3,7 @@ from ticktack import fitting
 import pickle
 
 
-mf, soln = fitting.fit_event(None, path=snakemake.input[0], production_model='control_points',
+mf, soln = fitting.fit_event(None, path=snakemake.input[0], production_model='control_points', sampler="optimisation",
                             cbm_model=snakemake.params.cbm_model,
                             hemisphere=snakemake.params.hemisphere,)
 with open(snakemake.output[0], 'wb') as file:
