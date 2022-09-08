758111578103f14dfbaeba8c415db429a9a95766
qingyuanzhang3 <zhangqingyuan03@icloud.com>
2022-09-08 20:37:44 +1000

    update to work with log uniform prior

--- scripts/individual_sample.py
+++ scripts/individual_sample.py
@@ -11,7 +11,8 @@ if snakemake.params.production_model == "simple_sinusoid_sharp":
     mle = sf.chain_summary(chain, 12, mle=True)
     @jit
     def simple_sinusoid_sharp(t, *args):
-        start_time, duration, area = jnp.array(list(args)).reshape(-1)
+        start_time, log_duration, log_area = jnp.array(list(args)).reshape(-1)
+        duration, area = 10**log_duration, 10**log_area
         height = sf.super_gaussian(t, start_time, duration, area)
         production = sf.steady_state_production + mle[-1] * sf.steady_state_production * jnp.sin(
             2 * np.pi / 11 * t + mle[3] * 2 * np.pi / 11) + height
@@ -22,7 +23,8 @@ else:
     mle = sf.chain_summary(chain, 12, mle=True)
     @jit
     def simple_sinusoid_prolonged(t, *args):
-        start_time, duration, area = jnp.array(list(args)).reshape(-1)
+        start_time, log_duration, log_area = jnp.array(list(args)).reshape(-1)
+        duration, area = 10 ** log_duration, 10 ** log_area
         height = sf.super_gaussian(t, start_time, duration, area)
         production = sf.steady_state_production + mle[-1] * sf.steady_state_production * jnp.sin(
             2 * np.pi / 11 * t + mle[3] * 2 * np.pi / 11) + height
@@ -30,9 +32,9 @@ else:
     model = simple_sinusoid_prolonged
 
 sf.compile_production_model(model=model)
-params = np.array([775, 1. / 12, 81./ 12,])
-low_bounds = np.array([775 - 5, 1 / 52., 0.])
-up_bounds = np.array([775 + 5, 5., 15.])
+params = np.array([775, np.log10(1. / 12), np.log10(81./ 12)])
+low_bounds = np.array([775 - 5, np.log10(1 / 52.), -2])
+up_bounds = np.array([775 + 5, np.log10(5.), 1.5])
 chain = sf.MarkovChainSampler(params, sf.log_joint_likelihood, burnin=1000,
                                 production=1000, args=(low_bounds, up_bounds))
 np.save(snakemake.output[0], chain)
