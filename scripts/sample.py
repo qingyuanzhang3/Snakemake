9a423a14692ee9aa607c6a97b3a76e3aef7c91aa
qingyuanzhang3 <zhangqingyuan03@icloud.com>
2022-09-12 17:53:05 +1000

    Update sample.py

--- scripts/sample.py
+++ scripts/sample.py
@@ -7,6 +7,9 @@ if snakemake.params.event == "5259BCE":
 elif snakemake.params.event == "775AD-Prolonged":
     params = np.array([-1.74123467e-03, 7.73721759e+02, 2.52047467e-01, 2.27947073e+00,
                        7.87020467e-01, -1.51734656e+00])
+elif snakemake.params.event == "993AD":
+    params = np.array([-5.32783967e-04,  9.92948483e+02,  np.log10(1.12824990e+00),
+    6.04790666e+00,  np.log10(3.10722315e+00),  np.log10(3.82991199e-02)])
 else:
     params = None
 mf, sampler = fitting.fit_event(snakemake.params.year,
