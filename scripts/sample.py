0123466dbd43d847a76b20eb97e2e0e09054d9f0
qingyuanzhang3 <zhangqingyuan03@icloud.com>
2022-09-26 18:59:44 +1000

    Update sample.py

--- scripts/sample.py
+++ scripts/sample.py
@@ -2,8 +2,8 @@ import numpy as np
 from ticktack import fitting
 
 if snakemake.params.event == "5259BCE":
-    params = np.array([-8.28506168e-03, -5.25965399e+03,  np.log10(4.66141251e-01),
-    3.09932956e+00, np.log10(6.24373446e+00),  np.log10(2.51351680e-02)])
+    params = np.array([-9.87302625e-03, -5.25961987e+03, -7.53614127e-01,  1.43895132e+00,
+        8.53135328e-01, -1.22434240e+00])
 elif snakemake.params.event == "775AD-Prolonged":
     params = np.array([-1.74123467e-03, 7.73721759e+02, 2.52047467e-01, 2.27947073e+00,
                        7.87020467e-01, -1.51734656e+00])
